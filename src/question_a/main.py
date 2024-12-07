12f164b88ae85b219682a2e810754f540f348193
prisillygirl <priscilla.jenkins2@g.austincc.edu>
2024-12-07 03:26:13 +0000

    question a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,41 @@
 #add import
+
+from question_a import Stock 
+
+def stock_purchase_history():
+   
+    aapl = Stock('AAPL', 'Apple Inc')
+    cat = Stock('CAT', 'Caterpillar')
+    ek = Stock('EK', 'Eastman Kodak')
+    goog = Stock('GOOG', 'Google')
+    msft = Stock('MSFT', 'Microsoft')
+
+    stk_dict = { 
+                        aapl.get_symbol() : aapl.get_company_name(),
+                        cat.get_symbol() : cat.get_company_name(),
+                        ek.get_symbol() : ek.get_company_name(),
+                        goog.get_symbol() : goog.get_company_name(),
+                        msft.get_symbol() : msft.get_company_name()}
+    for symbol, name in stk_dict.items():
+       print(f"{symbol:<10} {name:<20}")
+    
+
+def menu():
+    print("""     MENU   
+1-Display stock purchase history
+2-Exit
+          """)
+    choice = input('INPUT SELECTION 1 OR 2: ')
+    if choice == "1":
+        stock_purchase_history()
+        menu()
+    elif choice == '2':
+        print("Exiting the program.")
+        
+    else:
+            print("INVALID. Please input selection 1 or 2.")
+            menu()
+
+def main():
+     menu()
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,33 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+class Stock:
+    def __init__(self, symbol, company_name):
+
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+    def get_company_name(self):
+        return self.__company_name 
+    
+    def stock_purchase_history(self):
+        
+
+        aapl = Stock("AAPL", "Apple Inc")
+        cat = Stock("CAT", "Caterpillar")
+        ek = Stock("EK", "Eastman Kodak")
+        goog = Stock("GOOG", "Google")
+        msft = Stock("MSFT", "Microsoft")
+
+        stk_dict = {
+        aapl.get_symbol : aapl.get_company_name,
+        cat.get_symbol : cat.get_company_name,
+        ek.get_symbol : ek.get_company_name,
+        goog.get_symbol : goog.get_company_name,
+        msft.get_symbol : msft.get_company_name,
+        }
+        for stock in stk_dict:
+            print (stock)

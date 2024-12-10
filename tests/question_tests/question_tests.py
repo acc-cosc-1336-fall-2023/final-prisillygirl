75900bb6877ce9ae17cb5cecf57e8e5d24a3ff84
prisillygirl <priscilla.jenkins2@g.austincc.edu>
2024-12-10 06:41:15 +0000

    Final Commit

--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -1,10 +1,14 @@
 #write function tests here, don't add input('') statements here!
 import unittest
 
+from question_a.question_a import test_config
 from question_d.question_d import get_most_likely_ancestor_consensus
 
+class Test_Config(unittest.TestCase):
 
-class TestAncestorConsensus(unittest.TestCase):
+    def test_question_a_config(self):
+        self.assertEqual(True, test_config())
+        
 
     def test_get_most_likely_ancestor_consensus(self):
         dna_string1 = "GATATATGCATATACTT"

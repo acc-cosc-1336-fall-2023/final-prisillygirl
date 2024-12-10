c64a40af6181df44188a87915b28ca23f657172b
prisillygirl <priscilla.jenkins2@g.austincc.edu>
2024-12-10 06:38:11 +0000

    Final Commit

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,2 +1,34 @@
-#add import
 
+from question_d import get_most_likely_ancestor_consensus
+
+
+def main():
+    while True:
+       
+        dna_string1 = input("Enter a DNA string between 9 and 16 characters: ")
+        
+        if len(dna_string1) < 9 or len(dna_string1) > 16:
+            print("DNA string must be between 9 and 16 characters. Please enter between 9 and 16 characters.")
+            continue
+        
+        
+        dna_string2 = input("Enter a DNA substring of exactly 4 characters: ")
+   
+        if len(dna_string2) != 4:
+            print("DNA substring must be exactly 4 characters. Please enter 4 characters.")
+            continue
+        
+   
+        result = get_most_likely_ancestor_consensus(dna_string1, dna_string2)
+        
+    
+        if result:
+            print("Positions of the substring:", " ".join(map(str, result)))
+        else:
+            print(f"The substring '{dna_string2}' was not found in the DNA string.")
+   
+        user_choice = input("Do you want to continue? (y/n): ").strip().lower()
+        if user_choice != 'yes':
+            print("Exiting the program...Bye!")
+            break
+main()
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,18 @@
 #write functions here, don't add input('') statements here!
+
+def get_most_likely_ancestor_consensus(dna_string1, dna_string2):
+     
+    positions = []
+    for i in range(len(dna_string1) - len(dna_string2) + 1):
+        positions.append(i + 1)
+
+    return tuple(positions)
+
+dna_string1 = "GATATATGCATATACTT"
+dna_string2 = "ATAT"
+
+positions = get_most_likely_ancestor_consensus(dna_string1, dna_string2)
+
+
+for position in positions:
+    print(position)
